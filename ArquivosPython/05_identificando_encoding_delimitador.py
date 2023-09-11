ddd2d0715dc74f6ac9b526f3dbedbc6fa3dea268
DBA Assists <83104598+dbaassists@users.noreply.github.com>
2023-09-10 21:40:04 -0300

    Alteração arquivo

--- ArquivosPython/05_identificando_encoding_delimitador.py
+++ ArquivosPython/05_identificando_encoding_delimitador.py
@@ -5,6 +5,36 @@
 import pandas as pd
 import bibliotecas
 
+# %%
+
+def detecta_encoding(arquivo):
+
+    with open(arquivo, 'rb') as arquivo_encoding:
+
+        resultado_encoding = chardet.detect(arquivo_encoding.read())
+
+        enconding_detectado = resultado_encoding['encoding']
+
+    return enconding_detectado
+
+#df.head()
+
+# %% 
+
+# Detectar o Delimitador
+
+def detecta_delimitador(arquivo):
+
+    with open(arquivo, 'r', newline='') as arquivo_delimitador:
+
+        detector_delimitador = csv.Sniffer()
+
+        amostra_delimitador = arquivo_delimitador.read(1024)
+
+        delimitador_detectado = detector_delimitador.sniff(amostra_delimitador).delimiter
+
+    return delimitador_detectado
+
 # %% 
 
 arquivo = 'C:\Temp\Python_YT\Arquivo_Exemplo\Arquivos_Candidatos\consulta_cand_2022_AC.csv'
